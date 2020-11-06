adb2ef3850aa002997f26e30ee06a2fe951e5137
YutoSuzuki1024 <yuto.suzuki1024@gmail.com>
2020-11-06 21:26:25 +0900

    ruby hash(1)

--- lib/sample.rb
+++ lib/sample.rb
@@ -1,12 +1,2 @@
-fruits = ["apple", "orange", "melon"]
-count = 0
-fruits.each do |fruit|
-	print "#{fruit}は好きですか？=>"
-	answer = "いいえ"
-	puts answer
-
-	count += 1
-	redo if answer != "はい" && count < 2
-
-	count = 0
-end
+currencies = {japan: :yen, us: :doller, india: :rupee}
+puts currencies[:us]
