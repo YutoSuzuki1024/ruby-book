0726c061748ef0543da04b67fdb64eaf1fa1a7fe
YutoSuzuki1024 <yuto.suzuki1024@gmail.com>
2020-11-07 19:01:11 +0900

    ruby hash(2)

--- lib/sample.rb
+++ lib/sample.rb
@@ -1,2 +1,13 @@
-currencies = {japan: :yen, us: :doller, india: :rupee}
-puts currencies[:us]
+def buy_burger(menu, drink: true, potato: true)
+	puts "#{menu}を購入"
+
+	if drink
+		puts "ドリンクを購入"
+	end
+
+	if potato
+		puts "ポテトを購入"
+	end
+end
+
+buy_burger("fish", potato: true)
