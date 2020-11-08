d26ead5a732eb873546b555ce056fb2473a3dc1c
YutoSuzuki1024 <yuto.suzuki1024@gmail.com>
2020-11-08 18:57:37 +0900

    ruby hash(3)

--- lib/sample.rb
+++ lib/sample.rb
@@ -1,13 +1,4 @@
-def buy_burger(menu, drink: true, potato: true)
-	puts "#{menu}を購入"
-
-	if drink
-		puts "ドリンクを購入"
-	end
-
-	if potato
-		puts "ポテトを購入"
-	end
+def user_exist?
+	user = find_user
+	
 end
-
-buy_burger("fish", potato: true)
