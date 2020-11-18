e30f000346dc828368e3f83b473a9f93f773664c
YutoSuzuki1024 <yuto.suzuki1024@gmail.com>
2020-11-18 11:03:00 +0900

    ruby Module(3)

--- lib/sample.rb
+++ lib/sample.rb
@@ -1,29 +1,29 @@
+module Loggable
+	def log(text)
+		puts "[LOG] #{text}"
+	end
+end
+
 class Product
+	include Loggable
+
 	def title
 		log "title is called."
 		"A great movie"
 	end
-	
-	private
-	def log(text)
-		puts "[LOG] #{text}"
-	end
 end
 
 class User
+	include Loggable
+
 	def name
 		log "name is called."
 		"Alice"
 	end
-	
-	private
-	def log(text)
-		puts "[LOG] #{text}"
-	end
 end
 
 product = Product.new
-product.title
+puts product.title
 
 user = User.new
-user.name
+puts user.name
