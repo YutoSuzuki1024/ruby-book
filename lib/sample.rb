2311eff88f6cc58349572dc9682205e333dd3f08
YutoSuzuki1024 <yuto.suzuki1024@gmail.com>
2020-11-26 20:56:22 +0900

    ruby Exception Handling(1)

--- lib/sample.rb
+++ lib/sample.rb
@@ -1,19 +1,15 @@
-module Baseball
-	class Second
-		def initialize(player, uniform_number)
-			@player = player
-			@uniform_number = uniform_number
-		end
+puts "Start."
+module Greeter
+	def hello
+		"hello"
 	end
 end
 
-module Clock
-	class Second
-		def initialize(digits)
-			@digits = digits
-		end
-	end
+begin
+	greeter = Greeter.new
+rescue
+	puts "例外が発生したが、このまま続行する"
 end
 
-puts Baseball::Second.new("Alice", 13)
-puts Clock::Second.new(13)
+
+puts "End."
